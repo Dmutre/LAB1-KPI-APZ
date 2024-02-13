5c4c997ce20ab0d9673183db769a906c5afd5b68
Dmutre <dmytro.lesko@gmail.com>
2024-02-13 18:17:17 +0100

    commit Dmutre

--- server.go
+++ server.go
@@ -43,7 +43,6 @@ func getTime(w http.ResponseWriter, req *http.Request) {
 
 func main() {
 	fmt.Println("Hello server");
-  fmt.Println("Hello, world!");
   http.HandleFunc("/time", getTime);
   http.HandleFunc("/", serverHandle);
 

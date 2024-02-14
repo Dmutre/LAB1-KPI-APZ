53f1374e9a596c52ac51c198ace1201c463a8d22
Dmutre <dmytro.lesko@gmail.com>
2024-02-14 21:18:34 +0100

    Update server.go

--- server.go
+++ server.go
@@ -1,50 +1,48 @@
 package main
 
 import (
-  "encoding/json"
-  "net/http"
-  "time"
-  "fmt"
+	"encoding/json"
+	"net/http"
+	"time"
 )
 
 type ServerStatus struct {
-  Message string `json:"message"`
+	Message string `json:"message"`
 }
 
 func serverHandle(w http.ResponseWriter, req *http.Request) {
-  status := ServerStatus{Message: "Server is running!"}
-  
-  w.Header().Set("Content-Type", "application/json")
-  w.WriteHeader(http.StatusOK)
-  json.NewEncoder(w).Encode(status)
+	status := ServerStatus{Message: "Server is running!"}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(status)
 }
 
 type TimeResponse struct {
-  CurrentTime string `json:"time"`
+	CurrentTime string `json:"time"`
 }
 
 func getTime(w http.ResponseWriter, req *http.Request) {
-  currentTime := time.Now();
+	currentTime := time.Now()
 
-  response := TimeResponse{
-    CurrentTime: currentTime.Format(time.RFC3339),
-  };
+	response := TimeResponse{
+		CurrentTime: currentTime.Format(time.RFC3339),
+	}
 
-  jsonResponse, err := json.Marshal(response);
-  if err != nil {
-    http.Error(w, err.Error(), http.StatusInternalServerError)
-    return
-  };
+	jsonResponse, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-  w.Header().Set("Content-Type", "application/json");
+	w.Header().Set("Content-Type", "application/json")
 
-  w.Write(jsonResponse);
+	w.Write(jsonResponse)
 }
 
 func main() {
-  http.HandleFunc("/time", getTime);
-  http.HandleFunc("/", serverHandle);
+	http.HandleFunc("/time", getTime)
+	http.HandleFunc("/", serverHandle)
 
-  http.ListenAndServe(":8795", nil);
-  fmt.Println("Server is running on port 8795");
+	http.ListenAndServe(":8795", nil)
 }
